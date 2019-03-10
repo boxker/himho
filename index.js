38180618dd62725edf435f6da2b4f04273893c2d
boxker <icjb@foxmail.com>
2019-03-10 20:02:34 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    var urlall = new Array();
+    urlall[0] = 'https://www.baidu.com/s?word=';
+    urlall[1] = 'https://cn.bing.com/search?q=';
+    urlall[2] = 'http://www.sogou.com/tx?query=';
+    urlall[3] = 'https://www.so.com/s?q=';
+    urlall[4] = 'https://www.google.com/search?q=';
+
+    div1 =
+        '<iframe width="100%" height="100%" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="yes" allowtransparency="yes" src="';
+    div2 = '" ></frame>';
+
+    function go(str, whi) {
+        var url = "";
+        if (whi == "1") {
+            url = urlall[0];
+        } else if (whi == "2") {
+            url = urlall[1];
+        } else if (whi == "3") {
+            url = urlall[2];
+        } else if (whi == "4") {
+            url = urlall[3];
+        } else if (whi == "5") {
+            url = urlall[4];
+        } else console.log("null");
+
+        $("#search").html(div1 + url + str + div2);
+        //$("#searchboxcont").attr("placeholder",str);
+        $("#searchboxcont").attr("value", str);
+        $("#home").hide(500);
+        $("#search").show(100);
+        $("#searchbox").show(100);
+    }
+
+    $("#searchbox").hide();
+    $("#search").hide();
+    $("#searbut").click(function () {
+        var str = $("#searcont").val();
+        var whi = $("#which").val();
+
+        go(str, whi);
+    });
+    $("#searboxbut").click(function () {
+        var str = $("#searchboxcont").val();
+        var whi = $("#which").val();
+
+        go(str, whi);
+    });
+    $("#searcont").keydown(function (e) {
+        var curKey = e.which;
+        if (curKey == 13) {
+            $("#searbut").click();
+            return false;
+        }
+    });
+    $("#searchboxcont").keydown(function (e) {
+        var curKey = e.which;
+        if (curKey == 13) {
+            $("#searboxbut").click();
+            return false;
+        }
+    });
+});
