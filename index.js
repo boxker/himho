6bf794f8f1ef1560e459ef5e03e1127bd1e821f3
boxker <icjb@foxmail.com>
2019-03-26 21:33:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,24 @@
             '<iframe width="100%" height="100%" frameborder="no" border="0" marginwidth="0" marginheight="0" scrolling="yes" allowtransparency="yes" src="';
         div2 = '" ></frame>';
 
+        function setRa() {
+            var ra = localStorage.getItem("ra");
+            if (ra == null) {
+                localStorage.setItem("ra", "long");
+            }
+            if (ra == "short") {
+                $("#r1").attr("checked", true);
+                $(".more").hide();
+                //$("footer").attr("class", "text-center container footer navbar-fixed-bottom");
+            } else {
+                $("#r2").attr("checked", true);
+                $(".more").show();
+                $("footer").attr("class", "text-center container footer");
+            }
+        }
+
+        setRa();
+
         function go(str, whi) {
             var url = "";
             if (whi == "1") {
@@ -19,14 +37,14 @@
                 url = urlall[1];
             } else if (whi == "3") {
                 url = urlall[2];
-                $("#which").attr("value",1);
+                $("#which").attr("value", 1);
                 window.open(url + str);
                 url = urlall[0];
             } else if (whi == "4") {
                 url = urlall[3];
             } else if (whi == "5") {
                 url = urlall[4];
-                $("#which").attr("value",1);
+                $("#which").attr("value", 1);
                 window.open(url + str);
                 url = urlall[0];
             } else console.log("null");
@@ -71,6 +89,7 @@
 
         if (mobile_flag) {
             $("#where").hide();
+            $("#navtop-which").hide();
         }
 
         $("#searbut").click(function () {
@@ -98,5 +117,36 @@
                 $("#searboxbut").click();
                 return false;
             }
+        });
+        $('input:radio[name="optionsRadios"]').change(function () {
+            //console.log("hi");
+            if ($("input[name='optionsRadios']:checked").val() == "short") {
+                $(".more").hide();
+                //$("footer").attr("class", "text-center container footer navbar-fixed-bottom");
+                localStorage.ra = "short";
+            } else {
+                $(".more").show();
+                $("footer").attr("class", "text-center container footer");
+                localStorage.ra = "long";
+            }
+            //setRa();
+        });
+        $.getJSON("https://www.apiopen.top/journalismApi", function (data, status) {
+            var str = "";
+            var strtop = '<div class="panel panel-default"><div class="panel-body">';
+            var strbot = '</div></div>';
+            for (var i = 0;i < data["data"]["toutiao"].length ;i++){
+                str = str + strtop;
+                if (data["data"]["toutiao"][i]["picInfo"][0]!=null)
+                    str = str + '<p><img height=100 width=auto src="' + data["data"]["toutiao"][i]["picInfo"][0]["url"] + '" />';
+                else{
+                    str = str + '<p>';
+                }
+                str = str + '<b><h4><a href="' + data["data"]["toutiao"][i]["link"] + '">' + data["data"]["toutiao"][i]["title"] + '</a></h4></b>';
+                str = str + '<p>' + data["data"]["toutiao"][i]["category"] + ' ' + data["data"]["toutiao"][i]["ptime"] + '</p></p>'
+                str = str + strbot;
+            }
+            console.log(str);
+            $("#news").html(str);
         });
     });
